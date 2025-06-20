6a72625c02b85fd7818d38518de536ed5591acb2
Subramani Thangavel <subramani.thangavel@firstcitizens.com>
2025-06-20 19:57:27 +0000

    final

--- audit_fct_customer_order.sql
+++ audit_fct_customer_order.sql
@@ -1,14 +1,17 @@
 {% set old_relation = adapter.get_relation(
-      database = target.database,
-      schema = "DBT_STHANGAVEL",
-      identifier = "customer_orders"
+    database=target.database,
+    schema="DBT_STHANGAVEL",
+    identifier="legacy_customer_orders",
 ) -%}
 
-{% set dbt_relation = ref('fct_customer_orders') %}
+{% set dbt_relation = ref("fct_customer_orders") %}
 
-{{ audit_helper.compare_and_classify_relation_rows(
-    a_relation = old_relation,
-    b_relation = dbt_relation,
-    primary_key_columns = "order_id"
-    
-) }}
+{% if execute %}
+
+    {{
+        audit_helper.compare_relations(
+            a_relation=old_relation, b_relation=dbt_relation, primary_key="order_id"
+        )
+    }}
+
+{% endif %}
